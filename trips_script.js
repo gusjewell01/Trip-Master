1053c55cfa0bf65e0c55a7028105995a7c98049d
gusjewell01 <87389165+gusjewell01@users.noreply.github.com>
2021-11-26 11:37:15 -0500

    Trip object, stops/users array, stop directions
    Trip object contains stops array that shows directions between stops, can only be used after trip is created.

--- trips_script.js
+++ trips_script.js
@@ -1,9 +1,69 @@
 
 
+    
     $("#new_trip").click(function(){
         $("#new_trip_body").html();
     });
     
+    let trips = [];
+
+    var tripCreated = false;
+    function Trip(user, name, origin, destination, minBudget, maxBudget, interests, start, end) {
+        this.user = user;
+        this.users = [];
+        this.name = name;
+        this.origin = origin;
+        this.destination = destination;
+        this.minBudget = minBudget;
+        this.maxBudget = maxBudget;
+        this.interests = interests;
+        this.start = start;
+        this.end = end;
+        this.stops = [origin];
+
+        this.addStop = function(stop) {
+            this.stops.push(stop);
+        }
+
+        this.addUser = function(newUser) {
+            this.users.push(newUser);
+        }
+        
+        this.getUser = function() {
+            return this.user;
+        }
+        this.getUsers = function() {
+            return this.users;
+        }
+        this.getName = function() {
+            return this.name;
+        }
+        this.getOrigin = function() {
+            return this.origin;
+        }
+        this.getDestination = function() {
+            return this.destination;
+        }
+        this.getMinBudget = function() {
+            return this.minBudget;
+        }
+        this.getMaxBudget = function() {
+            return this.maxBudget;
+        }
+        this.getInterests = function() {
+            return this.interests;
+        }
+        this.getStart = function() {
+            return this.start;
+        }
+        this.getEnd = function() {
+            return this.end;
+        }
+        this.getStops = function() {
+            return this.stops;
+        }
+    }
+
     $("#create").click(function() {
         
         let name = $("#tripname").val();
@@ -11,11 +71,30 @@
         let destination = $("#to").val();
         let minBudget = $("#minBudget").val();
         let maxBudget = $("#maxBudget").val();
-        let interests = $("#interests").val();
+        let interests = $("#ints").val();
         let start = $("#startDate").val();
         let end = $("#endDate").val();
-        alert("Trip Created!\nName: " + name +"\nOrigin: " + origin + "\nDestination: " + destination + "\n" + "Budget: $" + minBudget + 
-        " - $" + maxBudget +"\nInterests: " + interests +"\nStart Date: " + start + "\nEnd Date: " + end);
+        
+
+        var Trip1 = new Trip("example user", name, origin, destination, minBudget, maxBudget, interests, start, end);
+
+        // const output = document.querySelector('#tripOutput');
+        
+        // output.innerHTML = "<div class='alert-info'>From: " + document.getElementById("from").value + ".<br />To: " + document.getElementById("to").value + ".<br /> Driving distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duration <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
+        
+        tripCreated = true;
+        alert("Trip Created!");
+        trips.push(Trip1);
+        
+
+    });
+
+    $("#addUser").click(function() {
+        if (tripCreated) {
+            trips[0].addUser($("#userInfo").val());
+            alert("New user added! Current users on trip: " + trips[0].getUsers());
+        } else 
+        alert("No Active Trip!");
     });
 
     var myLatLng = { lat: 35.397, lng: -80.844 };
@@ -48,8 +127,8 @@
         directionsService.route(request, function (result, status) {
             if (status == google.maps.DirectionsStatus.OK) {
     
-                const output = document.querySelector('#output');
-                output.innerHTML = "<div class='alert-info'>From: " + document.getElementById("from").value + ".<br />To: " + document.getElementById("to").value + ".<br /> Driving distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duration <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
+                const output = document.querySelector('#tripOutput');
+                output.innerHTML = "<br><div class='alert-info'>From: " + document.getElementById("from").value + ".<br />To: " + document.getElementById("to").value + ".<br /> Driving distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duration <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
     
                 //display route
                 directionsDisplay.setDirections(result);
@@ -80,5 +159,45 @@
     
     var input2 = document.getElementById("to");
     var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
+
+    var input3 = document.getElementById("stop");
+    var autocomplete3 = new google.maps.places.Autocomplete(input3, options);
+
+
+    let stopCounter = 0;
+
+    function addStop() {
+        if (tripCreated) {
+        stopCounter = stopCounter + 1;
+        trips[0].addStop(document.getElementById("stop").value);
+
+        var request = {
+            origin: trips[0].getStops()[stopCounter-1],
+            destination: trips[0].getStops()[stopCounter],
+            travelMode: google.maps.TravelMode.DRIVING, //WALKING, BYCYCLING, TRANSIT
+            unitSystem: google.maps.UnitSystem.IMPERIAL
+        }
+        
+        directionsService.route(request, function (result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+    
+                const output = document.querySelector('#tripOutput');
+                output.innerHTML = "<br><div class='alert-info'>From: " + trips[0].getStops()[stopCounter-1] + ".<br />To: " + trips[0].getStops()[stopCounter] + ".<br /> Driving distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duration <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
+    
+                //display route
+                directionsDisplay.setDirections(result);
+            } else {
+                //delete route from map
+                directionsDisplay.setDirections({ routes: [] });
+                
+                map.setCenter(myLatLng);
+    
+                output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
+            }
+        });
+    } else 
+    alert("No Active Trip!");
+        
+    }
 
 
